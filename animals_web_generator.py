1e69eba1d211274056d470ac622afee852578879
Nico Schulze <nico.schulze1602@gmail.com>
2025-04-22 14:58:06 +0200

    Add files via upload
    add files

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,31 +2,36 @@ import json
 import pprint
 
 def load_data(file_path):
-  """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-
-animals_data = load_data('animals_data.json')
-
-for animal in animals_data:
-  locations = animal['locations']
-  characteristics = animal['characteristics']
-
-  print(f'\nName: {animal['name']}')
-  print(f'Diet: {characteristics['diet']}')
-  print(f'Location: {locations[0]}')
-  try:
-      print(f'Type: {characteristics['type']}')
-  except KeyError:
-      continue
+data = load_data('animals_data.json')
 
 
+def print_animals_data(data):
+  output = ''
+  for animal_data in data:
+    output += '\n'
 
-"""pprint.pprint(animals_data[0])
-print("- - - - - - - - - - -")
-pprint.pprint(animals_data[1]['name'])
-print("- - - - - - - - - - -")
-pprint.pprint(animals_data[5]['characteristics'])
-print("- - - - - - - - - - -")
-pprint.pprint(animals_data[10]['locations'])"""
+    # Name (nur wenn vorhanden)
+    if 'name' in animal_data:
+      output += f"Name: {animal_data['name']}\n"
+
+    # Characteristics (nur wenn vorhanden)
+    characteristics = animal_data.get('characteristics')
+    if characteristics:
+      if 'diet' in characteristics:
+        output += f"Diet: {characteristics['diet']}\n"
+      if 'type' in characteristics:
+        output += f"Type: {characteristics['type']}\n"
+
+    # Locations (nur wenn Liste vorhanden & nicht leer)
+    locations = animal_data.get('locations')
+    if locations and isinstance(locations, list) and len(locations) > 0:
+      output += f"Location: {locations[0]}\n"
+
+  return output
+
+
+#template = load_data('animals_template.html')
+print_animals_data()
