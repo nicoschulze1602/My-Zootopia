8d2f6b435d66155e691860f192ad8df0c8604928
Nico Schulze <nico.schulze1602@gmail.com>
2025-04-29 13:16:58 +0200

    improve visualization

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,27 +10,34 @@ data = load_data('animals_data.json')
 def get_animals_data():
     """returns a string with the animal"""
     output = ''
-    for animal in data:
-        output += '<li class="cards__item">\n'
-        output += f'  <div class="card__title">{animal.get("name", "Unbekannt")}</div>\n'
-        output += '  <p class="card__text">\n'
+    for animal_obj in data:
+        output += serialize_animal(animal_obj)
+    return output
 
-        characteristics = animal.get('characteristics', {})
-        diet = characteristics.get('diet')
-        if diet:
-            output += f'    <strong>Diet:</strong> {diet}<br/>\n'
+def serialize_animal(animal_obj):
+    output = '<li class="cards__item">\n'
+    output += '  <div class="card">\n'
 
-        locations = animal.get('locations', [])
-        if locations:
-            output += f'    <strong>Location:</strong> {locations[0]}<br/>\n'
+    name = animal_obj.get("name", "Unbekannt")
+    output += f'    <div class="card__header"><h2 class="card__title">{name}</h2></div>\n'
+    output += '    <div class="card__body">\n'
 
-        type_ = characteristics.get('type')
-        if type_:
-            output += f'    <strong>Type:</strong> {type_}<br/>\n'
+    characteristics = animal_obj.get('characteristics', {})
+    diet = characteristics.get('diet')
+    if diet:
+        output += f'      <p><strong>Diet:</strong> {diet}</p>\n'
 
-        output += '  </p>\n'
-        output += '</li>\n'
+    locations = animal_obj.get('locations', [])
+    if locations:
+        output += f'      <p><strong>Location:</strong> {locations[0]}</p>\n'
 
+    type_ = characteristics.get('type')
+    if type_:
+        output += f'      <p><strong>Type:</strong> {type_}</p>\n'
+
+    output += '    </div>\n'
+    output += '  </div>\n'
+    output += '</li>\n'
     return output
 
 animals_data = get_animals_data().strip()
@@ -39,6 +46,6 @@ with open('animals_template.html', 'r') as file:
     template = file.read()
     new_html = template.replace('__REPLACE_ANIMALS_INFO__', animals_data)
 
-with open('animals.html', 'w') as file:
+with open('animals.html', 'w', encoding="utf-8") as file:
     file.write(new_html)
 
